b8d48d2bf7559a0fe403d1193fa58eb68183e5cc
Zachary Zadell <zacharyz@gmail.com>
2026-01-17 09:22:38 -0800

    feat: Upgrade Next.js to v16.1.2, migrate `next.config` from JavaScript to TypeScript, and update related dependencies.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        punycode: false,
+      };
+    }
+    return config;
+  },
+  turbopack: {},
 };
 
 export default nextConfig;
